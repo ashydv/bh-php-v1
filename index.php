e3f619a56b86b5b5f7afe3e6ed4a15532bab57e2
ashokyadav <ashok.yadav2@wipro.com>
2019-02-25 15:39:39 +0530

    commit 03

--- index.php
+++ index.php
@@ -111,7 +111,16 @@
     </section>
     <footer>
         <p class="secondlastline">
-          This page is being served by instance id -
+          This page is being served by the <?php
+            $url = "http://169.254.169.254/latest/meta-data/instance-id";
+            $instance_id = file_get_contents($url);
+            echo "Instance ID: <u>" . $instance_id . "</u><br/>";
+            ?>
+            running in the Availability <?php
+            $url = "http://169.254.169.254/latest/meta-data/placement/availability-zone";
+            $zone = file_get_contents($url);
+            echo "Zone: <u>" . $zone . "</u><br/>";
+          ?>
         </p>
         <p class="lastline">
           Thank you for visiting. Happy thumping.
