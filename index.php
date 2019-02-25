a7e680ef36c76ddd4270495ef96a9b16a99d79e5
ashokyadav <ashok.yadav2@wipro.com>
2019-02-25 15:47:20 +0530

    commit 04

--- index.php
+++ index.php
@@ -114,12 +114,12 @@
           This page is being served by the <?php
             $url = "http://169.254.169.254/latest/meta-data/instance-id";
             $instance_id = file_get_contents($url);
-            echo "Instance ID: <u>" . $instance_id . "</u><br/>";
+            echo "Instance ID: <b>" . $instance_id . "</b><br/>";
             ?>
             running in the Availability <?php
             $url = "http://169.254.169.254/latest/meta-data/placement/availability-zone";
             $zone = file_get_contents($url);
-            echo "Zone: <u>" . $zone . "</u><br/>";
+            echo "Zone: <b>" . $zone . "</b><br/>";
           ?>
         </p>
         <p class="lastline">
